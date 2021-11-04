44c6c74ed1089c4d80df99ad000f0020204f6136
bschiwal <brandon.schiwal.@gmail.com>
2021-11-04 16:16:05 -0500

    Update Readme.

--- MappingTest.R
+++ MappingTest.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(ggplot2)
 library(tmap)
 
+
 shp <- readRDS(gzcon(url("https://github.com/mgimond/ES218/blob/gh-pages/Data/maine_tracts.Rds?raw=true")))
 dat <- read.csv("http://mgimond.github.io/ES218/Data/maine_commute.csv")
 
@@ -11,8 +12,8 @@ summary(shp)
 summary(dat)
  head(shp)
  head(dat)
- ggplot(shp2) + geom_sf(aes(fill=Commute))
-ggplot(shp2)  +geom_sf()
+ ggplot(shp2)+ geom_sf(aes(fill=Commute))
+ggplot(shp2)+geom_sf()
 
 
 load(url("https://github.com/mgimond/ES218/blob/gh-pages/Data/counties48.RData?raw=true"))
@@ -77,3 +78,73 @@ cnty2.df1 <- inner_join(cnty2, df1, by=c("fips" = "FIPS") )
 head(cnty2.df1)
 tm_shape(cnty2.df1) + tm_fill(col = "B20004001", palette = "Greens") +
   tm_legend(outside = TRUE) 
+
+
+###ADD Max Row
+fltdem<- cnty2.df4 %>%
+  select(diff_2020,total_2020,win2020)%>%
+  filter(win2020=="DEM")
+plot(fltdem$diff_2020,fltdem$total_2020)
+
+quantile(fltdem$diff_2020, probs=c(1,.75,.5,.25,0))
+
+fltgop<- cnty2.df4 %>%
+  select(diff_2020,total_2020,win2020)%>%
+  filter(win2020=="GOP")
+plot(fltgop$diff_2020,fltgop$total_2020)
+
+quantile(fltgop$diff_2020, probs=c(1,.75,.5,.25,0))
+summary(cnty2.dem)
+
+cnty2.max<-c(max(cnty2.df4$dem_2020),0)
+cnty2.min<-0
+cnty2.dem1<-cnty2.df4$dem_2020
+cnty2.gop1<-cnty2.df4$gop_2020
+cnty2.dem2<-append(cnty2.dem1,cnty2.max)
+cnty2.gop2<-append(cnty2.gop1,cnty2.max)
+
+cnty2.dem3<- normalize(cnty2.dem2,method="range",range=c(0,1))
+cnty2.gop3<- normalize(cnty2.gop2,method="range",range=c(0,1))
+cnty2.dem<-cnty2.dem3[1:3068]
+cnty2.gop<-cnty2.gop3[1:3068]
+
+cnty2.df5<-cnty2.df4%>%
+  mutate(gop_2020= cnty2.gop)%>%
+  mutate(dem_2020=cnty2.dem)%>%
+  mutate(diff_2020=gop_2020-dem_2020)
+
+tst<-cnty2.df5%>%
+  filter( diff_2020==0)
+
+ggplot(cnty2.df5)+
+  geom_sf(aes(fill=diff_2020_bin))+ scale_fill_gradient2(low="#2832c2", high = "#d21404", mid="#a86cc1", breaks = c(-1,-.66,-.33,0,.33,.66,1))
+
+cnty2.df5<-cnty2.df5%>%
+  mutate(diff_2020_bin= case_when(
+    diff_2020 >0 &diff_2020<.33~.33,
+    diff_2020 >.33 &diff_2020<.66~.66,
+    diff_2020 >.66 ~.1,
+    diff_2020 <0 &diff_2020>-.33~-.33,
+    diff_2020 <.33 &diff_2020>-.66~-.66,
+    diff_2020 <.66~-1,
+    TRUE ~0
+  ))%>%
+  mutate(diff_2020_binlab= case_when(
+    diff_2020 >0 &diff_2020<.33~"Weak GOP",
+    diff_2020 >.33 &diff_2020<.66~"Mid GOP",
+    diff_2020 >.66 ~"Strong GOP",
+    diff_2020 <0 &diff_2020>-.33~"Weak DEM",
+    diff_2020 <.33 &diff_2020>-.66~"Mid DEM",
+    diff_2020 <.66~"Strong DEM",
+    TRUE ~"None"
+  ))
+
+ggplot(cnty2.df5)
+  geom_sf(aes(fill=diff_2020_binlab))
+
+
+cnty2.df5%>%
+  filter(win2020=="DEM")%>%
+  select(diff_2020)%>%
+  quantile(diff_2020,probs=c(0,.25,.5,.75,1))
+  quantile(diff_2020)
--- README.md
+++ README.md
@@ -11,3 +11,5 @@ future portfolio.
 in 'PresidentialResults08-20.R' I am testing various ways at analyzing county data in the past presidential elections. 
 
 MappingTest.R is a test project I found to help me learn how to generate maps in R.
+
+![2020 US voting Map](./2020USvotemap.jpg)
